ca9d60b3f4e9c5926fe3c2d32c09a9b4c1becfdc
Rocco_RK <rkrocco119@gmail.com>
2025-12-04 13:02:39 +0300

    Personnalisation Cleanfolio

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,24 +1,31 @@
-import { header } from '../../portfolio'
-import Navbar from '../Navbar/Navbar'
-import './Header.css'
-
-const Header = () => {
-  const { homepage, title } = header
-
-  return (
-    <header className='header center'>
-      <h3>
-        {homepage ? (
-          <a href={homepage} className='link'>
-            {title}
-          </a>
-        ) : (
-          title
-        )}
-      </h3>
-      <Navbar />
-    </header>
-  )
+export const header = {
+  homepage: 'https://RoccoRK119.github.io',
+  title: 'Rocco RK',
 }
 
-export default Header
+export const about = {
+  name: 'Rocco RK',
+  role: 'Développeur Web Full-Stack',
+  description: 'Je suis passionné par le développement web et les technologies modernes.',
+  resume: 'CV_RoccoRK.pdf',
+}
+
+export const skills = ['HTML', 'CSS', 'JavaScript', 'React', 'Python', 'Django']
+
+export const projects = [
+  {
+    name: 'Portfolio React',
+    description: 'Mon portfolio réalisé avec React et TailwindCSS.',
+    url: 'https://RoccoRK119.github.io',
+    repo: 'https://github.com/RoccoRK119/RoccoRK119.github.io',
+  },
+]
+
+export const contact = {
+  email: 'rocco@example.com',
+}
+
+export const footer = [
+  { name: 'GitHub', url: 'https://github.com/RoccoRK119' },
+  { name: 'LinkedIn', url: 'https://linkedin.com/in/roccork' },
+]
